afa6b8687f0125b54604d7b4f83e16a3bad916de
m13hack <119548847+m13hack@users.noreply.github.com>
2024-10-23 14:42:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-echo "[+] Installing Python dependencies..."
-pip install fpdf
+echo "[+] Updating package list..."
+sudo apt-get update
 
-echo "[+] Installation complete!"
+echo "[+] Installing Python3..."
+sudo apt-get install -y python3
+
+echo "[+] Installing pip for Python3..."
+sudo apt-get install -y python3-pip
+
+echo "[+] Installing Python modules: wmi, subprocess, datetime..."
+pip3 install wmi subprocess datetime
+
+echo "[+] Installing common system tools: net-tools, curl, wget, auditd..."
+sudo apt-get install -y net-tools curl wget auditd
+
+echo "[+] Installing PowerShell..."
+sudo apt-get install -y powershell
+
+echo "[+] Installing Windows compatibility libraries (Wine)..."
+sudo apt-get install -y wine
+
+echo "[+] Installing Firewall (iptables)..."
+sudo apt-get install -y iptables
+
+echo "[+] Installing cert-utils and firewall..."
+sudo apt-get install -y  iptables 
+
+
